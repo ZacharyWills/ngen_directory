fc19fdd308c3f33161122305c47e8ce7aabd2645
ZacharyWills <60660437+ZacharyWills@users.noreply.github.com>
2024-04-26 15:46:56 -0600

    Add files via upload

---
+++ bookmarks.js
@@ -0,0 +1,32 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "NGEN Model Framework",
+    links: [
+      { name: "Public Code Repository", url: "https://github.com/noaa-owp/ngen" },
+      { name: "Documentation", url: "https://noaa-owp.github.io/ngen/" },
+    ],
+  },
+  {
+    title: "HydroFabric",
+    links: [
+      { name: "Introduction", url: "https://noaa-owp.github.io/hydrofabric/articles/01-intro-deep-dive.html" },
+      { name: "Public Code Repository", url: "https://github.com/NOAA-OWP/hydrofabric" },
+      { name: "Data", url: "https://www.lynker-spatial.com/#hydrofabric/" },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      { name: "DocuHub", url: "https://docs.ciroh.org/" },
+      { name: "News", url: "https://docs.ciroh.org/docs/products/tools/nextgeninabox/news" },
+      { name: "Tutorial", url: "https://github.com/CIROH-UA/NGIAB-CloudInfra" },
+    ],
+  },
+  {
+    title: "Examples",
+    links: [
+      { name: "Colorado School of Mines - Clear Creek", url: "https://github.com/mines-ciroh/clear_creek_demo" },
+    ],
+  },
+];
